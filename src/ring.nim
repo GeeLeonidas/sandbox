0efa58734d01123f10ac75642b03a3ad3ff89dcd
Guilherme Leoi <geeleonidas@pm.me>
2022-12-28 18:00:49 -0300

    Finish base `RingBuffer` implementation

--- src/ring.nim
+++ src/ring.nim
@@ -1,3 +1,5 @@
+import std / strformat
+
 type RingBuffer*[T] = object
   raw: ptr UncheckedArray[T]
   start, length: Natural
@@ -13,25 +15,42 @@ proc `=destroy`*[T](x: var RingBuffer[T]) =
 
 proc add*[T](ring: var RingBuffer[T], value: T) =
   if unlikely(ring.length >= ring.capacity):
-    discard # Copy
+    return # Copy
   let newIndex = (ring.start + ring.length) mod ring.capacity
   ring.raw[newIndex] = value
   inc ring.length
 
-proc popFront*[T](ring: RingBuffer[T]): T = 
+proc popFront*[T](ring: var RingBuffer[T]): T = 
   when not defined(danger):
     if unlikely(ring.length == 0):
       let exception = new IndexDefect
       exception.msg = "index out of bounds, the container is empty"
       raise exception
+  dec ring.length
+  result = ring.raw[ring.start]
+  ring.start = (ring.start + 1) mod ring.capacity
 
-proc popBack*[T](ring: RingBuffer[T]): T =
+proc popBack*[T](ring: var RingBuffer[T]): T =
   when not defined(danger):
     if unlikely(ring.length == 0):
       let exception = new IndexDefect
       exception.msg = "index out of bounds, the container is empty"
       raise exception
+  dec ring.length
+  result = ring.raw[(ring.start + ring.length) mod ring.capacity]
+
+proc `[]`*[T](ring: RingBuffer[T], index: Natural): T =
+  when not defined(danger):
+    if unlikely(index >= ring.length):
+      let exception = new IndexDefect
+      exception.msg = fmt"index {index} not in 0 .. {ring.length - 1}"
+      raise exception
+  result = ring.raw[(ring.start + index) mod ring.capacity]
 
 when isMainModule:
   var test = default(RingBuffer[int])
+  for i in 1..10:
+    test.add(i)
+  echo test.popFront()
   echo test.popBack()
+  echo test[0]
