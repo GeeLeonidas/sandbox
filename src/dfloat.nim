989d81f7d89ae7c1f9231b4186f9e138a5f1f23a
Guilherme Leoi <geeleonidas@pm.me>
2022-12-28 14:49:51 -0300

    Move `when isMainModule` block

--- src/dfloat.nim
+++ src/dfloat.nim
@@ -48,3 +48,23 @@ proc `/=`*(a: var DirtyFloat, b: DirtyFloat) =
 proc `/`*(a, b: DirtyFloat): DirtyFloat =
   result = a
   result /= b
+
+when isMainModule:
+  const
+    N = 10_000
+    A = 3.333'df
+  var testNum = 0'df
+
+  for i in 1..N:
+    testNum += A # I love float imprecision
+
+  let
+    rightValue = toFloat(A) * N
+    obtainedValue = toFloat(clean testNum)
+    dirtyValue = toFloat(testNum)
+
+  echo rightValue
+  echo obtainedValue
+  echo rightValue == obtainedValue
+  echo dirtyValue
+  echo rightValue == dirtyValue
