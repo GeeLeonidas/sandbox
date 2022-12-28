6e69b73b0eba857ade6db97eeab725e2d7c9d4d7
Guilherme Leoi <geeleonidas@pm.me>
2022-12-28 20:40:01 -0300

    Add dynamic capacity

--- src/ring.nim
+++ src/ring.nim
@@ -5,17 +5,45 @@ type RingBuffer*[T] = object
   start, length: Natural
   capacity: Positive
 
-proc default*[T](_: typedesc[RingBuffer[T]]): RingBuffer[T] =
-  result = RingBuffer[T](capacity: 16)
+proc newRingBufferOfCap*[T](capacity: Positive): RingBuffer[T] =
+  result = RingBuffer[T](capacity: capacity)
   result.raw = cast[ptr UncheckedArray[T]](createU(T, result.capacity))
+
+proc default*[T](_: typedesc[RingBuffer[T]]): RingBuffer[T] =
+  newRingBufferOfCap[T](8)
 
 proc `=destroy`*[T](x: var RingBuffer[T]) =
   if likely(not x.raw.isNil):
     dealloc x.raw
 
+proc `=copy`*[T](x: var RingBuffer[T], y: RingBuffer[T]) =
+  x = RingBuffer[T](capacity: y.capacity)
+  x.raw = cast[ptr UncheckedArray[T]](createU(T, x.capacity))
+  moveMem(x.raw, y.raw, y.capacity)
+
+proc `[]`*[T](ring: RingBuffer[T], index: Natural): T =
+  when not defined(danger):
+    if unlikely(index >= ring.length):
+      let exception = new IndexDefect
+      exception.msg = fmt"index {index} not in 0 .. {ring.length - 1}"
+      raise exception
+  result = ring.raw[(ring.start + index) mod ring.capacity]
+
+proc `[]`*[T](ring: RingBuffer[T], index: BackwardsIndex): T =
+  ring[ring.length + index.int - 1]
+
+proc grow[T](ring: var RingBuffer[T]) =
+  let newRaw = cast[ptr UncheckedArray[T]](createU(T, 2 * ring.capacity))
+  for i in 0 ..< ring.length:
+    newRaw[i] = ring[i]
+  dealloc ring.raw
+  ring.raw = newRaw
+  ring.start = 0
+  ring.capacity *= 2
+
 proc add*[T](ring: var RingBuffer[T], value: T) =
   if unlikely(ring.length >= ring.capacity):
-    return # Copy
+    grow ring
   let newIndex = (ring.start + ring.length) mod ring.capacity
   ring.raw[newIndex] = value
   inc ring.length
@@ -39,18 +67,14 @@ proc popBack*[T](ring: var RingBuffer[T]): T =
   dec ring.length
   result = ring.raw[(ring.start + ring.length) mod ring.capacity]
 
-proc `[]`*[T](ring: RingBuffer[T], index: Natural): T =
-  when not defined(danger):
-    if unlikely(index >= ring.length):
-      let exception = new IndexDefect
-      exception.msg = fmt"index {index} not in 0 .. {ring.length - 1}"
-      raise exception
-  result = ring.raw[(ring.start + index) mod ring.capacity]
-
 when isMainModule:
   var test = default(RingBuffer[int])
-  for i in 1..10:
-    test.add(i)
+  for n in 1..15:
+    test.add(n)
   echo test.popFront()
   echo test.popBack()
-  echo test[0]
+  
+  var testTwo = test
+  testTwo.add(15)
+  echo testTwo[^0]
+  echo test[^0]
