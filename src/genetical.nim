57c79fc900c4dc152b5db57ca8d153e549a6c25e
Guilherme Leoi <geeleonidas@pm.me>
2023-10-15 02:09:14 -0300

    Add genetical primitives

--- src/genetical.nim
+++ src/genetical.nim
@@ -1,10 +1,57 @@
-import std / [sequtils, random, sugar, algorithm]
+import std / [sequtils, random, sugar, algorithm, bitops, strformat]
+
+type Chromosome = distinct uint
+
+{.push inline.}
+proc value(x: Chromosome): uint = x.uint
+proc asInt(x: Chromosome): int = cast[int](x.value)
+proc asFloat(x: Chromosome): float = cast[float](x.value)
+proc `$`(x: Chromosome): string = &"{$typeof x}({x.uint:#X})"
+{.pop.}
+
+proc `~>`(x, y: Chromosome): (Chromosome, Chromosome) =
+  const
+    ChromosomeBits = 8 * sizeof Chromosome
+    CrossoverBits = ChromosomeBits div 3
+    MaxBounds = ChromosomeBits - CrossoverBits
+  let
+    maskInner = block:
+      var
+        res = 0'u
+        count = 0
+      while count < CrossoverBits:
+        let bitIdx = rand 0..MaxBounds
+        if res.testBit(bitIdx):
+          continue
+        res.flipBit(bitIdx)
+        inc count
+      res
+    maskOuter = not maskInner
+    chromoA = (x.value and maskInner) or (y.value and maskOuter)
+    chromoB = (y.value and maskInner) or (x.value and maskOuter)
+  return (chromoA.Chromosome, chromoB.Chromosome)
+
+type
+  Individual[N: static int] = array[N, Chromosome]
+  Population[S, N: static int] = array[S, Individual[N]]
+
+proc initIndividual[N: static int]: Individual[N] =
+  for i in 0..<N:
+    result[i] = (rand uint).Chromosome
+
+proc initPopulation[S, N: static int]: Population[S, N] =
+  for i in 0..<S:
+    result[i] = initIndividual[N]()
 
 when isMainModule:
   randomize()
   const
     ParamRange = -1e9..1e9
     PopSize    = 1000
+    ChromNum   = 2
+  var population = initPopulation[PopSize, ChromNum]()
+  echo population
+  #[
   let
     coef = rand ParamRange
     disp = rand ParamRange
@@ -39,3 +86,4 @@ when isMainModule:
       bestL = sortedPop[0].l
   echo bestL
   echo bestA, ' ', bestB
+  ]#
