dd603386a74e45dd65d15cceb52cb7bc9b8514f6
Guilherme Leoi <geeleonidas@pm.me>
2023-10-29 13:01:19 -0300

    Rework symbol groups

--- src/genetical.nim
+++ src/genetical.nim
@@ -1,51 +1,73 @@
 import std / [ random, sequtils, sugar, math ]
 
 const
-  ValidOps  = ['+', '-', '*', '/', 'P', 'L']
-  ValidVars = ['a']
+  UnaryOps   = ['P', 'L']
+  BinaryOps  = ['+', '-', '*', '/']
+  TernaryOps = []
+  Terminals  = ['a']
+
+proc concatAllSymbols: seq[char] =
+  result = @Terminals
+  when UnaryOps.len > 0:
+    result = result.concat(@UnaryOps)
+  when BinaryOps.len > 0:
+    result = result.concat(@BinaryOps)
+  when TernaryOps.len > 0:
+    result = result.concat(@TernaryOps)
 
 proc initGene(headSize: Positive; maxParamCount = 2.Positive): string =
+  let opsAndTerminals = concatAllSymbols()
   for _ in 1..headSize:
-    result.add sample(@ValidOps.concat(@ValidVars))
+    result.add sample(opsAndTerminals)
   let tailSize = headSize * (maxParamCount - 1) + 1
   for _ in 1..tailSize:
-    result.add sample(ValidVars)
+    result.add sample(Terminals)
 
 proc evalGene(gene: string, input: varargs[float]): (string, float) =
   assert gene != "", "Shouldn't pass a empty gene!"
-  if gene[0] in ValidOps:
-    let
-      (leftGene, leftRes) = evalGene(gene.substr(1), input)
-      (rightGene, rightRes) = evalGene(leftGene, input)
-    case gene[0]
-    of '+':
-      return (rightGene, leftRes + rightRes)
-    of '-':
-      return (rightGene, leftRes - rightRes)
-    of '*':
-      return (rightGene, leftRes * rightRes)
-    of '/':
-      return (rightGene, leftRes / rightRes)
-    of 'P':
-      return (rightGene, pow(leftRes, rightRes))
-    of 'L':
-      return (rightGene, log(leftRes, rightRes))
-    else:
-      raise newException(ValueError, "Unimplemented operator `" & gene[0] & '`')
-  else:
-    return (gene.substr(1), input[ValidVars.find(gene[0])])
+  when TernaryOps.len > 0:
+    if gene[0] in TernaryOps:
+      # TODO: implement ternary logic
+      raise newException(ValueError, "Unimplemented ternary operator `" & gene[0] & '`') 
+  when BinaryOps.len > 0:
+    if gene[0] in BinaryOps:
+      let
+        (leftGene, leftRes) = evalGene(gene.substr(1), input)
+        (rightGene, rightRes) = evalGene(leftGene, input)
+      case gene[0]
+      of '+':
+        return (rightGene, leftRes + rightRes)
+      of '-':
+        return (rightGene, leftRes - rightRes)
+      of '*':
+        return (rightGene, leftRes * rightRes)
+      of '/':
+        return (rightGene, leftRes / rightRes)
+      else:
+        raise newException(ValueError, "Unimplemented binary operator `" & gene[0] & '`')
+  when UnaryOps.len > 0:
+    if gene[0] in UnaryOps:
+      let (leftGene, leftRes) = evalGene(gene.substr(1), input)
+      case gene[0]
+      of 'P':
+        return (leftGene, pow(2, leftRes))
+      of 'L':
+        return (leftGene, log2(leftRes))
+      else:
+        raise newException(ValueError, "Unimplemented unary operator `" & gene[0] & '`')
+  return (gene.substr(1), input[Terminals.find(gene[0])])
 
 proc mutateGene(gene: string, headSize: int; rate = 2 / gene.len): string =
   result = gene
-  let opsAndTerminals = @ValidOps.concat(@ValidVars)
+  let opsAndTerminals = concatAllSymbols()
   if opsAndTerminals.len > 1:
     for idx in 0..<headSize:
       if rate >= rand 1.0:
         result[idx] = sample(opsAndTerminals.filterIt(it != result[idx]))
-  if ValidVars.len > 1:
+  if Terminals.len > 1:
     for idx in headSize..<gene.len:
       if rate >= rand 1.0:
-        result[idx] = sample(@ValidVars.filterIt(it != result[idx]))
+        result[idx] = sample(@Terminals.filterIt(it != result[idx]))
 
 proc fitness(ind: string; input, expected: openArray[float]; considerParsimony = false): float =
   var
