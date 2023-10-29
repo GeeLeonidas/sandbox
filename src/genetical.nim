b1c9935a8254e7c19867a2535c631faa0cd95cda
Guilherme Leoi <geeleonidas@pm.me>
2023-10-29 00:13:18 -0300

    Use P-GEP approach

--- src/genetical.nim
+++ src/genetical.nim
@@ -1,125 +1,145 @@
-import std / [sequtils, random, sugar, algorithm, bitops, strformat, math]
+import std / [ random, sequtils, sugar, math ]
 
-type Chromosome = distinct uint # TODO: ditch bitset approach
-template value(x: Chromosome): uint {.dirty.} = x.uint
+const
+  ValidOps  = ['+', '-', '*', '/', 'P', 'L']
+  ValidVars = ['a']
 
-{.push inline.}
-proc asInt(x: Chromosome): int = cast[int](x.value)
-proc asFloat(x: Chromosome): float = cast[float](x.value)
-proc `$`(x: Chromosome): string = &"{$typeof x}({x.uint:#X})"
-{.pop.}
+proc initGene(headSize: Positive; maxParamCount = 2.Positive): string =
+  for _ in 1..headSize:
+    result.add sample(@ValidOps.concat(@ValidVars))
+  let tailSize = headSize * (maxParamCount - 1) + 1
+  for _ in 1..tailSize:
+    result.add sample(ValidVars)
 
-proc `~>`(x, y: Chromosome): (Chromosome, Chromosome) =
-  const
-    ChromosomeBits = 8 * sizeof Chromosome
-    CrossoverBits = ChromosomeBits div 3
+proc evalGene(gene: string, input: varargs[float]): (string, float) =
+  assert gene != "", "Shouldn't pass a empty gene!"
+  if gene[0] in ValidOps:
+    let
+      (leftGene, leftRes) = evalGene(gene.substr(1), input)
+      (rightGene, rightRes) = evalGene(leftGene, input)
+    case gene[0]
+    of '+':
+      return (rightGene, leftRes + rightRes)
+    of '-':
+      return (rightGene, leftRes - rightRes)
+    of '*':
+      return (rightGene, leftRes * rightRes)
+    of '/':
+      return (rightGene, leftRes / rightRes)
+    of 'P':
+      return (rightGene, pow(leftRes, rightRes))
+    of 'L':
+      return (rightGene, log(leftRes, rightRes))
+    else:
+      raise newException(ValueError, "Unimplemented operator `" & gene[0] & '`')
+  else:
+    return (gene.substr(1), input[ValidVars.find(gene[0])])
+
+proc mutateGene(gene: string, headSize: int; rate = 2 / gene.len): string =
+  result = gene
+  let opsAndTerminals = @ValidOps.concat(@ValidVars)
+  if opsAndTerminals.len > 1:
+    for idx in 0..<headSize:
+      if rate >= rand 1.0:
+        result[idx] = sample(opsAndTerminals.filterIt(it != result[idx]))
+  if ValidVars.len > 1:
+    for idx in headSize..<gene.len:
+      if rate >= rand 1.0:
+        result[idx] = sample(@ValidVars.filterIt(it != result[idx]))
+  
+proc fitness(ind: string; input, expected: openArray[float]; considerParsimony = false): float =
+  var
+    sumError = 0.0
+    maxExecSize = 0
+  for i in 0..<input.len:
+    let (remains, output) = try: evalGene(ind, input[i]) except: return 0
+    if output.isNaN or output == Inf or output == NegInf: return 0
+    sumError += pow(expected[i] - output, 2)
+    let execSize = ind.len - remains.len
+    if maxExecSize < execSize:
+      maxExecSize = execSize
+  result = 1000 / (1 + round(sqrt(sumError)))
+  if considerParsimony:
+    result += 1e-4 * ind.len.float / maxExecSize.float
+
+proc sortElite(population: var openArray[string], score: var openArray[float]) =
+  var eliteIdx = 0
+  for idx in 0..<score.len:
+    if score[idx] > score[eliteIdx]:
+      eliteIdx = idx
   let
-    maskInner = block:
-      var
-        res = 0'u
-        count = 0
-      while count < CrossoverBits:
-        let bitIdx = rand BitsRange[uint]
-        if res.testBit(bitIdx):
-          continue
-        res.flipBit(bitIdx)
-        inc count
-      res
-    maskOuter = not maskInner
-    chromoA = (x.value and maskInner) or (y.value and maskOuter)
-    chromoB = (y.value and maskInner) or (x.value and maskOuter)
-  return (chromoA.Chromosome, chromoB.Chromosome)
+    tempInd = population[0]
+    tempScore = score[0]
+  population[0] = population[eliteIdx]
+  score[0] = score[eliteIdx]
+  population[eliteIdx] = tempInd
+  score[eliteIdx] = tempScore
 
-type
-  Individual[N: static int] = array[N, Chromosome]
-  Population[S, N: static int] = array[S, Individual[N]]
-
-proc initIndividual[N: static int]: Individual[N] =
-  for i in 0..<N:
-    result[i] = (rand uint).Chromosome
-
-proc initPopulation[S, N: static int]: Population[S, N] =
-  for i in 0..<S:
-    result[i] = initIndividual[N]()
-
-proc nextGeneration[S, N: static int](population: Population[S, N], score: array[S, SomeNumber]): Population[S, N] =
-  # Selection phase
-  for i in countup(0, S-4, 4):
-    let fittest =
-      if score[i] > score[i+1] and score[i] > score[i+2] and score[i] > score[i+3]:
-        population[i]
-      elif score[i+1] > score[i+2] and score[i+1] > score[i+3]:
-        population[i+1]
-      elif score[i+2] > score[i+3]:
-        population[i+2]
-      else:
-        population[i+3]
-    result[i div 4] = fittest
-  # Crossover phase
-  for i in countup(0, (S div 4) - 2, 2):
-    for j in 0..<N:
-      let
-        (childA, childB) = result[i][j] ~> result[i+1][j]
-        (childC, childD) = result[i][j] ~> result[i+1][j]
-        (childE, childF) = result[i][j] ~> result[i+1][j]
-        resIdx = (S div 4) + 3*i
-      result[resIdx    ][j] = childA
-      result[resIdx + 1][j] = childB
-      result[resIdx + 2][j] = childC
-      result[resIdx + 3][j] = childD
-      result[resIdx + 4][j] = childE
-      result[resIdx + 5][j] = childF
-  # Mutation phase
-  for i in 0..<S:
-    for j in 0..<N:
-      let bitIdx = rand BitsRange[uint]
-      result[i][j].value.flipBit(bitIdx)
+proc rouletteSelection(population: openArray[string], score: openArray[float]; withElitism = true): seq[string] =
+  if withElitism:
+    result.add population[0]
+  let sumScore = sum score
+  var selections = 0
+  while selections < population.len:
+    var
+      idx = 0
+      accum = 0.0
+      chosen = rand 0.0..sumScore
+    block roulette:
+      while true:
+        while score[idx] == 0:
+          inc idx
+          if idx == score.len:
+            break roulette
+        accum += score[idx]
+        if accum >= chosen:
+          break
+        inc idx
+      result.add population[min(idx, population.len - 1)]
+      inc selections
 
 when isMainModule:
-  randomize()
-  const ParamRange = -1e9..1e9
-  let
-    coef = rand ParamRange
-    disp = rand ParamRange
-    xVal = toSeq 1..100
-    data = mapIt xVal:
-      let itWithError = it.float * (1.0 + rand -0.01..0.01)
-      itWithError * coef + disp
-  echo coef, ' ', disp
   const
-    PopSize    = 1000
-    ChromNum   = 2
+    HeadSize = 14
+    SelectiontRange = 100
+
+  randomize()
+
+  let
+    xValues = collect:
+      for _ in 1..20:
+        rand 4.0..100.0
+    yValues = mapIt xValues:
+      pow(it, 2) + 2 * it
+
   var
-    population = initPopulation[PopSize, ChromNum]()
-    score: array[PopSize, float]
-  proc updateScore = 
-    for i in 0..<PopSize:
-      let
-        ind = population[i]
-        a = ind[0].asFloat
-        b = ind[1].asFloat
-      if a.isNaN or b.isNaN or a notin ParamRange or b notin ParamRange:
-        score[i] = -1
-        continue
-      let
-        predict = mapIt xVal:
-          it.float * a + b
-        loss = block:
-          let diff = collect:
-            for i in 0..<data.len:
-              max(data[i], predict[i]) - min(data[i], predict[i])
-          diff.foldl(a + b) / data.len.float
-      score[i] = 1 / loss
-  for epoch in 1..1000:
-    updateScore()
-    population = population.nextGeneration(score)
-  updateScore()
-  var
-    fittestIdx = 0
-    fittestScore = score[0]
-  for i in 1..<PopSize:
-    if fittestScore > score[i]:
-      fittestIdx = i
-      fittestScore = score[i]
-  echo fittestScore
-  echo population[fittestIdx][0].asFloat, ' ', population[fittestIdx][1].asFloat
+    population = collect:
+      for _ in 1..30:
+        initGene(HeadSize)
+    score = collect:
+      for idx in 0..<population.len:
+        fitness(population[idx], xValues, yValues)
+
+  while allIt(score, it <= SelectiontRange):
+    population = collect:
+      for _ in 1..30:
+        initGene(HeadSize)
+    score = collect:
+      for idx in 0..<population.len:
+        fitness(population[idx], xValues, yValues)
+
+  for epoch in 1..100:
+    sortElite(population, score)
+    population = rouletteSelection(population, score)
+
+    for idx in 1..<population.len:
+      population[idx] = mutateGene(population[idx], HeadSize)
+
+    score = collect:
+      for idx in 0..<population.len:
+        fitness(population[idx], xValues, yValues, score[0] > 999.5)
+    echo "\nGen ", epoch
+    echo "Avg.  score: ", sum(score) / population.len.float
+    echo "Elite score: ", score[0]
+    echo "\nBest individual"
+    echo population[0]
